ae22880aa6d4b8fc08bb7b7e619e776cb5f695a8
downfa11 <downfa11@naver.com>
2025-03-31 12:04:08 +0900

    feat: 관리자 페이지에서 dummy 생성기에 대한 API 구현

--- src/main/java/com/ns/solve/controller/problem/AdminController.java
+++ src/main/java/com/ns/solve/controller/problem/AdminController.java
@@ -1,9 +1,10 @@
 package com.ns.solve.controller.problem;
 
 import com.ns.solve.domain.dto.MessageEntity;
-import com.ns.solve.domain.dto.WargameProblemDto;
+import com.ns.solve.domain.dto.problem.wargame.WargameProblemDto;
 import com.ns.solve.domain.problem.Problem;
 import com.ns.solve.service.problem.ProblemService;
+import com.ns.solve.utils.DummyGenerator;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admin")
 public class AdminController {
     private final ProblemService problemService;
+    private final DummyGenerator dummyGenerator;
 
     @Operation(summary="검수를 위해 대기중인 문제 목록 조회", description = "제출된 문제를 검수하기 위해 대기중인 리스트를 조회합니다.")
     @GetMapping("/pending")
@@ -36,4 +38,19 @@ public class AdminController {
         WargameProblemDto checkedProblem = problemService.toggleProblemCheckStatus(id);
         return ResponseEntity.ok(new MessageEntity("Problem marked as checked", checkedProblem));
     }
+
+    @Operation(summary = "더미 데이터 생성", description = "테스트를 위한 더미 데이터를 생성합니다.")
+    @GetMapping("/generate-dummy")
+    public ResponseEntity<MessageEntity> generateDummyData(
+            @RequestParam(defaultValue = "1000") int userCount,
+            @RequestParam(defaultValue = "2000") int boardCount,
+            @RequestParam(defaultValue = "5") int boardCommentCount,
+            @RequestParam(defaultValue = "100") int problemCount,
+            @RequestParam(defaultValue = "3") int problemCommentCount,
+            @RequestParam(defaultValue = "10000") int solvedCount) {
+
+        dummyGenerator.generateDummyData(userCount, boardCount, boardCommentCount, problemCount, problemCommentCount, solvedCount);
+        return ResponseEntity.ok(new MessageEntity("Dummy data generated successfully", "succes"));
+    }
+
 }
