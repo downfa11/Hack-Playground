ee58ab7d89529b434bbe79471ebe34498aa14b09
downfa11 <downfa11@naver.com>
2025-04-06 01:23:47 +0900

    feat: 게시글 조회 로직에 대한 쿼리문 튜닝으로 성능 개선

--- src/main/java/com/ns/solve/repository/board/BoardCustomRepositoryImpl.java
+++ src/main/java/com/ns/solve/repository/board/BoardCustomRepositoryImpl.java
@@ -2,7 +2,7 @@ package com.ns.solve.repository.board;
 
 import com.ns.solve.domain.QBoard;
 import com.ns.solve.domain.QComment;
-import com.ns.solve.domain.dto.BoardSummary;
+import com.ns.solve.domain.dto.board.BoardSummary;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -22,23 +22,26 @@ public class BoardCustomRepositoryImpl implements BoardCustomRepository {
         this.qBoard = QBoard.board;
         this.qComment = QComment.comment;
     }
-
-
+    
     @Override
     public Page<BoardSummary> findBoardsByPage(Pageable pageable, boolean desc) {
         List<BoardSummary> boardSummaries = jpaQueryFactory
-                .selectFrom(qBoard)
+                .select(qBoard.id, qBoard.title, qBoard.creator.nickname, qBoard.updatedAt,
+                        qComment.count())
+                .from(qBoard)
+                .leftJoin(qBoard.commentList, qComment)
+                .groupBy(qBoard.id)
                 .orderBy(desc ? qBoard.createdAt.desc() : qBoard.createdAt.asc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch()
                 .stream()
-                .map(board -> new BoardSummary(
-                        board.getId(),
-                        board.getTitle(),
-                        board.getCreator().getNickname(),
-                        board.getUpdatedAt(),
-                        board.getCommentList().size())
+                .map(result -> new BoardSummary(
+                        result.get(qBoard.id),
+                        result.get(qBoard.title),
+                        result.get(qBoard.creator.nickname),
+                        result.get(qBoard.updatedAt),
+                        result.get(qComment.count()).intValue())
                 )
                 .toList();
 
@@ -48,37 +51,6 @@ public class BoardCustomRepositoryImpl implements BoardCustomRepository {
 
         return new PageImpl<>(boardSummaries, pageable, total);
     }
-
-//    @Override
-//    public Page<BoardSummary> findBoardsByPage(Pageable pageable, boolean desc) {
-//        QComment comment = QComment.comment;
-//
-//        List<BoardSummary> boardSummaries = jpaQueryFactory
-//                .select(qBoard.id, qBoard.title, qBoard.creator.nickname, qBoard.updatedAt,
-//                        qComment.count())
-//                .from(qBoard)
-//                .leftJoin(qBoard.commentList, qComment)
-//                .groupBy(qBoard.id)
-//                .orderBy(desc ? qBoard.createdAt.desc() : qBoard.createdAt.asc())
-//                .offset(pageable.getOffset())
-//                .limit(pageable.getPageSize())
-//                .fetch()
-//                .stream()
-//                .map(result -> new BoardSummary(
-//                        result.get(qBoard.id),
-//                        result.get(qBoard.title),
-//                        result.get(qBoard.creator.nickname),
-//                        result.get(qBoard.updatedAt),
-//                        result.get(qComment.count()).intValue())
-//                )
-//                .toList();
-//
-//        long total = jpaQueryFactory
-//                .selectFrom(qBoard)
-//                .fetchCount();
-//
-//        return new PageImpl<>(boardSummaries, pageable, total);
-//    }
 
 
 }
