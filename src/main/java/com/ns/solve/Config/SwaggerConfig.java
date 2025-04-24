dc4da71887f6440943bdb222890cb37eb647f6fd
downfa11 <downfa11@naver.com>
2025-04-24 23:07:35 +0900

    feat: Swagger HTTPS 이전

--- src/main/java/com/ns/solve/Config/SwaggerConfig.java
+++ src/main/java/com/ns/solve/Config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.ns.solve.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,8 +18,12 @@ public class SwaggerConfig {
                 .description("hello world")
                 .version("1.0.0");
 
+        Server server = new Server();
+        server.setUrl("https://ec2-3-34-134-27.ap-northeast-2.compute.amazonaws.com");
+
         return new OpenAPI()
-                .info(info);
+                .info(info)
+                .servers(List.of(server));
 
     }
 
