fa9022bdf28ea610a1d8d0c6a15c174d0fac6656
downfa11 <downfa11@naver.com>
2025-04-24 23:07:59 +0900

    feat: 서비스 도메인별 예외 처리 인터페이스 구현

---
+++ src/main/java/com/ns/solve/utils/exception/SolvedException.java
@@ -0,0 +1,30 @@
+package com.ns.solve.utils.exception;
+
+import com.ns.solve.utils.exception.ErrorCode.BaseErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class SolvedException extends RuntimeException {
+    private final BaseErrorCode errorCode;
+    private final String message;
+
+    public SolvedException(BaseErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+    }
+
+    public SolvedException(BaseErrorCode errorCode, String detail) {
+        this.errorCode = errorCode;
+        this.message = errorCode.getMessage() + "- " + detail;
+    }
+
+    @Override
+    public String getMessage() {
+        return "[%s] %s".formatted(errorCode.name(), message);
+    }
+
+    public HttpStatus getHttpStatus() {
+        return errorCode.getStatus();
+    }
+}
