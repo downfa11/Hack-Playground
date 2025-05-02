359691a8b31a7edc0c5979b5d6b1c4fcaa0eea79
downfa11 <downfa11@naver.com>
2025-05-03 01:22:31 +0900

    feat: Pod GC 구조 개선 및 ttl 튜닝

--- src/main/java/com/ns/solve/service/core/GCScheduler.java
+++ src/main/java/com/ns/solve/service/core/GCScheduler.java
@@ -10,15 +10,17 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class GCScheduler {
-    private static final long TTL_SECONDS = 3600; // 1hour
 
-    private final KubernetesAdapter kubernetesAdapter;
+    private static final long TTL_SECONDS = 1800; // 30min
+    private static final String WARGAME_NAMESPACE = "wargame";
+    private final KubernetesService kubernetesService;
 
 
     @Scheduled(fixedDelay = 10 * 60_000)
@@ -26,15 +28,20 @@ public class GCScheduler {
         log.info("[GC] Expired pod check start");
 
         try {
-            V1PodList podList = kubernetesAdapter.getPodList();
+            V1PodList podList = kubernetesService.getPodList(WARGAME_NAMESPACE);
 
             for (V1Pod pod : podList.getItems()) {
                 String podName = pod.getMetadata().getName();
+                Map<String, String> label = pod.getMetadata().getLabels();
+                String userId = label.get("userId");
+                String problemId = label.get("problemId");
+                String labelSelector = String.format("userId=%s,problemId=%s", userId, problemId);
 
                 try {
-                    Optional<Long> lastRequestTimestampOpt = kubernetesAdapter.getLatestRequestTimestamp(podName);
+                    Optional<Long> lastRequestTimestampOpt = kubernetesService.getLatestRequestTimestamp(WARGAME_NAMESPACE, podName);
                     if (lastRequestTimestampOpt.isEmpty()) {
-                        log.warn("[GC] Pod {} has no valid access time in logs.", podName);
+                        log.warn("[GC] Pod {} has no valid access time in logs. label: {}", podName, labelSelector);
+                        kubernetesService.deleteAllResourcesByLabel(WARGAME_NAMESPACE, labelSelector);
                         continue;
                     }
 
@@ -42,8 +49,8 @@ public class GCScheduler {
                     long elapsed = Duration.between(Instant.ofEpochMilli(lastRequestTimestamp), Instant.now()).getSeconds();
 
                     if (elapsed > TTL_SECONDS) {
-                        log.info("[GC] Deleting pod: {} (elapsed={}s > ttl={}s)", podName, elapsed, TTL_SECONDS);
-                        kubernetesAdapter.deletePod(podName);
+                        log.info("[GC] Deleting pod: {} (elapsed={}s > ttl={}s, label={})", podName, elapsed, TTL_SECONDS, labelSelector);
+                        kubernetesService.deleteAllResourcesByLabel(WARGAME_NAMESPACE, labelSelector);
                     } else {
                         log.info("[GC] Skipping pod: {} (elapsed={}s <= ttl={}s)", podName, elapsed, TTL_SECONDS);
                     }
